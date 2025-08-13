0c6d25bf092fb7ee06732cfa07ff4122850c4810
Mark Scannell <mfscannell@users.noreply.github.com>
2025-08-13 09:02:37 -0500

    Make breaking change

--- src/WorkshopDemo/WeatherForecast/WeatherForecast.cs
+++ src/WorkshopDemo/WeatherForecast/WeatherForecast.cs
@@ -1,5 +1,5 @@
 ﻿namespace WorkshopDemo.WeatherForecast;
-
+a
 public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
